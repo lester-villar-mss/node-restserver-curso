9df8134eb7b7b2d9e6ec73946b340ef8d7d07f95
Lester <ljvillarz@unc.edu.pe>
2019-12-17 09:31:05 -0500

    Npm start listo 4

--- server/server.js
+++ server/server.js
@@ -44,6 +44,6 @@ app.delete('/usuario', function(req, res) {
     res.json('delete usuario');
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     console.log('Escuchando el puerto', process.env.PORT);
 });
